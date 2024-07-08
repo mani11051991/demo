7e810abe1d3a1a36467191fbb4d24939e8650efa
mani11051991 <114678441+mani11051991@users.noreply.github.com>
2024-07-08 14:42:52 +0530

    Update userdata9.sh

--- userdata9.sh
+++ userdata9.sh
@@ -1,4 +1,6 @@
-
+#!/bin/bash
+sudo su
+yum update –y
 wget -O splunk-9.0.4.1-419ad9369127-linux-2.6-x86_64.rpm "https://download.splunk.com/products/splunk/releases/9.0.4.1/linux/splunk-9.0.4.1-419ad9369127-linux-2.6-x86_64.rpm"
 sudo yum install ./splunk-9.0.2-17e00c557dc1-linux-2.6-x86_64.rpm -y
 sudo bash
